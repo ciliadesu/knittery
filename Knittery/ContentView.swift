6737ee7051db85161ae86107948400e71ad0fd8b
Cecilia Valenti <ciliadesu@gmail.com>
2020-06-11 18:35:25 +0200

    Initial Commit

---
+++ Knittery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Knittery
+//
+//  Created by Cecilia Valenti on 2020-06-11.
+//  Copyright © 2020 Cecilia Valenti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
